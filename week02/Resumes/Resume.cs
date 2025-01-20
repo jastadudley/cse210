ee0307091454d9848f7af8ded3f660dce6ef94a9
jastadudley <jastamdudley@gmail.com>
2025-01-20 14:52:53 -0700

    Added code to resume.

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -1,2 +1,17 @@
 using System.Diagnostics.Contracts;
 
+public class Resumes
+{
+    public string _name;
+    public List <Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine ($"Name: {_name}");
+        Console.WriteLine("Jobs: ");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
