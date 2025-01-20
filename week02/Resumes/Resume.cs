e9f682cccaf8a05e099bd0d2f5c1c2e7a5f772b7
jastadudley <jastamdudley@gmail.com>
2025-01-20 13:25:27 -0700

    Completed Job class with Display method

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -3,12 +3,12 @@ using System.Diagnostics.Contracts;
 public class Job
 {
     public string _jobtitle;
-    public string _compny;
+    public string _company;
     public int _startYear;
     public int _endYear;
 
     public void Display()
     {
-        
+        Console.WriteLine($"{_jobtitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
