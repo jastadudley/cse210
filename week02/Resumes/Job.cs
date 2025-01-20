679cf20842f7137e1185945026d5d1b0e37440fb
jastadudley <jastamdudley@gmail.com>
2025-01-20 13:29:31 -0700

    Moved Job class code to correct file

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,12 @@
+public class Job
+{
+    public string _jobtitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobtitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
