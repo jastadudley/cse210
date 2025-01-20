ab8696d21c8d4d9e4a376acb7a332b24bc0580c7
jastadudley <jastamdudley@gmail.com>
2025-01-20 14:21:31 -0700

    Adding jobs to the program

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobtitle = "Hiring Manager";
+        job1._company = "Bulwark Exterminating";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobtitle = ""
     }
 }
--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -1,14 +1,2 @@
 using System.Diagnostics.Contracts;
 
-public class Job
-{
-    public string _jobtitle;
-    public string _company;
-    public int _startYear;
-    public int _endYear;
-
-    public void Display()
-    {
-        Console.WriteLine($"{_jobtitle} ({_company}) {_startYear}-{_endYear}");
-    }
-}
