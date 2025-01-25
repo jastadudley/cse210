378831971c98c1a7be9cbccfb55c7a6b87fb520c
jastadudley <jastamdudley@gmail.com>
2025-01-25 10:52:51 -0700

    Working on Scripture Class

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -10,10 +10,45 @@ class Program
         Reference reference =new Reference ("Mark 12:30-31",3,5,6);
         Scripture scripture = new Scripture ("And thou shalt love the Lord thy God with all thy heart, and with all thy soul, and with all thy mind, and with all thy strength: this is the first commandment. And the second is like, namely this, Thou shalt love thy neighbour as thyself. There is none other commandment greater than these.")
 
+        while (true)
+        {
+            Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
 
+
+            string input = Console.ReadLine();
+
+
+            if (input == "quit")
+            {
+                Console.Clear();
+                Console.WriteLine("");
+                break;
+            }
+        }
+
+        Console.Clear();
 
         public class Scripture
         {
+            private string _text;
+            private bool _hiddenWord;
+
+            public Word (string text)
+            {
+                _text = text;
+                _hiddenWord = false;
+            }
+
+            public void Hide()
+            {
+                _hiddenWord = true;
+            }
+
+            public void HiddenWord()
+            {
+                _hiddenWord = false;
+            }
+
 
         }
 
@@ -27,10 +62,13 @@ class Program
 
         public class Reference
         {
-
+            private string _book;
+            private int _chapter;
+            private int _startVerse;
+            private int _endVerse;
         }
 
 
     }
-    
+
 }
