76803a03b1b34cac7f3e7274591840db465dfc6b
jastadudley <jastamdudley@gmail.com>
2025-01-24 20:41:24 -0700

    Framework for ScriptureMemorizer project

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        Reference reference =new Reference ("Mark 12:30-31",3,5,6);
+        Scripture scripture = new Scripture ("And thou shalt love the Lord thy God with all thy heart, and with all thy soul, and with all thy mind, and with all thy strength: this is the first commandment. And the second is like, namely this, Thou shalt love thy neighbour as thyself. There is none other commandment greater than these.")
+
+
+
+        public class Scripture
+        {
+
+        }
+
+
+
+        public class World
+        {
+
+        }
+
+
+        public class Reference
+        {
+
+        }
+
+
     }
+    
 }
