99c4386112f25338ac050370e0a0408656759572
jastadudley <jastamdudley@gmail.com>
2025-01-25 18:32:07 -0700

    cleaning up errors and fixing text print glitches

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -2,63 +2,73 @@ using System;
 using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
 
-class Program
+namespace ScriptureMemorizer
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
 
-        Console.WriteLine("Choose a scripture youd like to memorise:");
-        Console.WriteLine(" ");
-        Console.WriteLine("1. Mark 12:30-31");
-        Console.WriteLine("2. Luke 6:27-28");
-        Console.WriteLine("3. Matthew 7:1-3");
-        Console.WriteLine("4. John 8:7");
-
-        string choice = Console.ReadLine();
-        Scripture selectedScripture;
-
-        if (choice == "1")
+        static void Main(string[] args)
         {
-            Reference reference =new Reference ("Mark 12:30-31",12,30,31);
-            selectedScripture = new Scripture ("And thou shalt love the Lord thy God with all thy heart, and with all thy soul, and with all thy mind, and with all thy strength: this is the first commandment. And the second is like, namely this, Thou shalt love thy neighbour as thyself. There is none other commandment greater than these.");
-        }
-        else if (choice == "2")
-        {
-            Reference reference =new Reference ("Luke 6:27-28",6,27,28);
-            selectedScripture = new Scripture ("But I say unto you which hear, Love your enemies, do good to them which hate you, Bless them that curse you, and pray for them which despitefully use you. ");
-        }
-        else if (choice == "3")
-        {
-            Reference reference =new Reference ("Matthew 7:1-3",7,1,3);
-            selectedScripture = new Scripture ("Judge not, that ye be not judged. For with what judgment ye judge, ye shall be judged: and with what measure ye mete, it shall be measured to you again. And why beholdest thou the mote that is in thy brother's eye, but considerest not the beam that is in thine own eye? ");
-        }
-        else if (choice == "4")
-        {
-            Reference reference =new Reference ("John 8:7",8,7);
-            selectedScripture = new Scripture ("So when they continued asking him, he lifted up himself, and said unto them, He that is without sin among you, let him first cast a stone at her.");
-        }
+            Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
 
+            Console.WriteLine("Choose a scripture youd like to memorise:");//This is my creative addition
+            Console.WriteLine(" ");
+            Console.WriteLine("1. Mark 12:30-31");
+            Console.WriteLine("2. Luke 6:27-28");
+            Console.WriteLine("3. Matthew 7:1-3");
+            Console.WriteLine("4. John 8:7");
 
-        while (true)
-        {
-            Console.WriteLine(scripture.GetDisplayText());
-            Console.WriteLine("Press enter to continue or type 'quit' to finish. To upload a different scripture type 'upload': ");
-            string input = Console.ReadLine();
+            string choice = Console.ReadLine();
+            Scripture selectedScripture= null;
 
-            if (input == "quit")
+            if (choice == "1")//This is my creative addition
             {
-                Console.Clear();
-                break;
+                Reference reference =new Reference ("Mark 12:30-31",12,30,31);
+                selectedScripture = new Scripture (reference,"And thou shalt love the Lord thy God with all thy heart, and with all thy soul, and with all thy mind, and with all thy strength: this is the first commandment. And the second is like, namely this, Thou shalt love thy neighbour as thyself. There is none other commandment greater than these.");
             }
-        }
-        Console.Clear();
-        scripture.HideRandomeWords(3);
+            else if (choice == "2")//This is my creative addition
+            {
+                Reference reference =new Reference ("Luke 6:27-28",6,27,28);
+                selectedScripture = new Scripture (reference,"But I say unto you which hear, Love your enemies, do good to them which hate you, Bless them that curse you, and pray for them which despitefully use you. ");
+            }
+            else if (choice == "3")//This is my creative addition
+            {
+                Reference reference =new Reference ("Matthew 7:1-3",7,1,3);
+                selectedScripture = new Scripture (reference,"Judge not, that ye be not judged. For with what judgment ye judge, ye shall be judged: and with what measure ye mete, it shall be measured to you again. And why beholdest thou the mote that is in thy brother's eye, but considerest not the beam that is in thine own eye? ");
+            }
+            else if (choice == "4")//This is my creative addition
+            {
+                Reference reference =new Reference ("John 8:7",8,7);
+                selectedScripture = new Scripture (reference,"So when they continued asking him, he lifted up himself, and said unto them, He that is without sin among you, let him first cast a stone at her.");
+            }
+            if (selectedScripture == null)
+            {
+                Console.WriteLine("Invalid choice. Try running again.");
+                return;
+            }
 
-        if (scripture.IsCompleatlyHidden())
-        {
-            Console.WriteLine(scripture.GetDisplayText());
-            break;
+
+            while (true)
+            {
+                Console.WriteLine(selectedScripture.GetDisplayText());
+                Console.WriteLine("Press enter to continue or type 'quit' to finish. To upload a different scripture type 'upload': ");
+                string input = Console.ReadLine();
+
+                if (input == "quit")
+                {
+                    Console.Clear();
+                    break;
+                }
+            
+                Console.Clear();
+                selectedScripture.HideRandomWords(3);
+
+                if (selectedScripture.IsCompleatlyHidden())
+                {
+                    Console.WriteLine(selectedScripture.GetDisplayText());
+                    break;
+                }
+            }
         }
 
 
@@ -66,7 +76,7 @@ class Program
         {
             private Reference _reference;
             private List<Word> _words;
-
+        
             public Scripture (Reference reference, string text)
             {
                 _reference = reference;
@@ -78,7 +88,7 @@ class Program
                     _words.Add(new Word(word));
                 }
             }
-            public void HideRandomeWords(int numberToHide)
+            public void HideRandomWords(int numberToHide)
             {
                 Random random = new Random();
                 int hiddenCount = 0;
@@ -95,13 +105,13 @@ class Program
             }
             public string GetDisplayText()
             {
-                string displayText = _reference.GetDisplayText() + " ";
+                string displayText = " ";
                 foreach (Word word in _words)
                 {
                     displayText += word.GetDisplayText() + " ";
                 }
 
-                return displayText.Trim();
+                return _reference.GetDisplayText() + " " + displayText.Trim();
 
             }
             public bool IsCompleatlyHidden()
@@ -116,8 +126,6 @@ class Program
                 return true;
             }
         }
-
-
 
 
 
@@ -171,16 +179,16 @@ class Program
                 _startVerse = verse;
                 _endVerse = -1;
             }
-            public Reference(string book, int chapter, int verse) 
+            public Reference(string book, int chapter, int startVerse, int endVerse) 
             {
                 _book = book;
                 _chapter = chapter;
-                _startVerse = verse;
+                _startVerse = startVerse;
                 _endVerse = endVerse;
             }
             public string GetDisplayText()
             {
-                if (_endVerse == 1)
+                if (_endVerse == -1)
                 {
                     return $"{_book} {_chapter}:{_startVerse}";
                 }
