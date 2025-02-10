0a7e1ce3af35de7fbdd02c8db099ba6952eda31a
jastadudley <jastamdudley@gmail.com>
2025-02-10 14:55:00 -0700

    Adding files and classes.

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,19 @@
+namespace Homework;
+
+public class Homework
+{
+    private string _studentName;
+    private string _topic;
+
+    public Homework(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummery()
+    {
+        return _studentName;
+        return _topic;
+    }
+}
