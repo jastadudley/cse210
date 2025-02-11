75b48caf02a8c8d0098cf0c3631417480f1ec04c
jastadudley <jastamdudley@gmail.com>
2025-02-11 16:49:27 -0700

    starting on Breathing Activity

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,1 +1,34 @@
 namespace Mindfulness;
+{
+    public class BreathingActivity : Activity
+    {
+        public class BreathingActivity()
+        : base ("BreathingActivity")
+
+        public void BreathingActivity(int duration);
+        {
+            DisplayStartingMessage();
+
+            int secondsElasped = 0;
+            While (secondsElasped < duration)
+            {
+                Console.WriteLine("Breath in...");
+                for (int i = 3; i > 0; i--)
+                {
+                    Console.Write(i + " ");
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine;
+
+                Console.WriteLine("...and breath out.");
+                for (int i = 3; i > 0; i--)
+                {
+                    Console.Write(i + " ");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+
+
+}
