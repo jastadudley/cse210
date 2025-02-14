95352364fbc1911a4f5df59738dfbb5ba54b8e46
jastadudley <jastamdudley@gmail.com>
2025-02-14 11:21:35 -0700

    Starting Listening Activity class

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,1 +1,33 @@
-namespace Mindfulness;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+namespace Mindfulness
+{
+    private class ListeningActivity : Activity
+    {
+        private List<string> prompts = new List<string>()
+        {
+            "blah blah",
+            "blah blah",
+            "blah blah",
+            "blah blah"
+        };
+
+        public ListeningActivity()
+        : base ("ListeningActivity","This Activity will help you slow down and listen.", 5 )
+        {}
+
+        
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
