3a8df9b49ce1812752062bdfa0f02a62f0ba21e6
jastadudley <jastamdudley@gmail.com>
2025-02-16 15:23:26 -0700

    creating program class and polishing it up

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+
+        private List<Shapes> shapes = new List<Shapes>();
+
+
+        shapes.Add(new Square(5,"Red"));
+        shapes.Add(new Square(9,"Pink"));
+        shapes.Add(new Square(2, "Blue"));
+
+
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"Color:{shape.Getcolor()}   Area:{shape.GetArea()}");
+
+
+        }
     }
 }
