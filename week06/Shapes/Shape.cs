d2799a55cd1730a31c654192bca49f7de16f9f17
jastadudley <jastamdudley@gmail.com>
2025-02-16 14:23:33 -0700

    Adding needed classes and starting on Shapes outline

---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Shapes
+{
+    public class Shape
+    {
+        private string _color;
+
+        public Shape(string color)
+        {
+            _color = color;
+        }
+
+
+        public string GetColor()
+        {
+
+        }
+
+        public void SetColor()
+        {
+
+        }
+
+
+
+        public virtual double GetArea()
+        {
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
