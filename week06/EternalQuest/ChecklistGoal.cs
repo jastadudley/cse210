c7d86775464dbb7262ddbf9d4e1f1c6599491bf5
jastadudley <jastamdudley@gmail.com>
2025-02-16 19:05:31 -0700

    outline for ChecklistGoal class.

--- week06/EternalQuest/ChecklistGoal.cs
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -1,7 +1,39 @@
 using System;
 namespace EternalQuest
 {
+    public class ChecklistGoal : Goal
+    {
+        private int _timesCompleated;
+        private int _target;
+        private int _bonusPoints;
 
+        public ChecklistGoal(string name, int points, int target, int bonusPoints)
+        :base(name, points, false)
+        {
+            _ame = name;
+            _points = points;
+            _target = target;
+            _bonusPoints = bonusPoints;
+        }
+
+        override string GetDetailsString()
+        {
+            return $"[ {(IsComplete() ? "X" : " ")} ] {GetName()} ({GetPoints()} pts)-- Compleated {_timesCompleated} / {_target} times";
+        }
+
+
+
+        override string RecordEvent()
+        {
+
+        }
+
+
+
+
+
+
+    }
 
 
 
