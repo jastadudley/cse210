73ccb1dce7282541c6958a6a34b7bc76e4b0ebbc
jastadudley <jastamdudley@gmail.com>
2025-02-16 17:24:45 -0700

    polishing up Goal class

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,7 +1,34 @@
 using System;
 namespace EternalQuest
 {
+    public abstract class Goal
+    {
+        private string _goal;
+        private int _points;
+        private bool _isComplete;
 
+
+
+        public Goal(string goal, int points, bool isComplete)
+        {
+            _goal = goal;
+            _points = points;
+            _isComplete = isComplete;
+        }
+
+
+
+
+        public GetDetailsString();
+
+        public RecordEvent();
+
+        public IsComplete()
+        {
+
+        }
+
+    }
 
 
 
