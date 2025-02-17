7ef9a2e371c249fb8c1cd38e051d56ac9f18682e
jastadudley <jastamdudley@gmail.com>
2025-02-16 19:41:24 -0700

    Framework for GoalManager class

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -1,9 +1,49 @@
 using System;
 namespace EternalQuest
 {
+    public class GoalManager
+    {
+        private List<Goals> _goals = new List<Goals>();
+        private int _score;
+
+        public GoalManager(string goals, int score)
+        {
+            _goals = goals;
+            _score = 0;
+        }
+
+        public void DisplayScore()
+        {
+
+        }
+
+
+        public void DisplayGoals()
+        {
+
+
+        }
+
+        public void CreateGoal()
+        {
+
+        }
+        public override void RecordEvent()
+        {
+
+        }
+
+        public void LoadGoals()
+        {
+
+        }
 
 
 
 
-    
+    }
+
+
+
+
 }
