654b289817f9cf97480b11754c957ab528b25940
jastadudley <jastamdudley@gmail.com>
2025-02-16 18:03:32 -0700

    Working my way through SimpleGoal

--- week06/EternalQuest/SimpleGoal.cs
+++ week06/EternalQuest/SimpleGoal.cs
@@ -1,7 +1,25 @@
 using System;
 namespace EternalQuest
 {
+    public class SimpleGoal : Goal
+    {
+        public SimpleGoal(string goal, int points)
+        :base(goal, points, false)
+        {
+        }
 
+        public override string GetDetailsString()
+        {
+            
+        }
+
+
+        public bool RecordEvent()
+        {
+            return isComplete = true;
+        }
+
+    }
 
 
 
