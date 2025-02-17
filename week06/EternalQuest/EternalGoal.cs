d4160f57685a370239e013fd40b635efe5b95d9b
jastadudley <jastamdudley@gmail.com>
2025-02-16 19:23:53 -0700

    Outline for EternalGoal, still fixing errors

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -1,9 +1,30 @@
 using System;
 namespace EternalQuest
 {
+    public class EternalGoal : Goal
+    {
+        public EternalGoal(string name, int points)
+        :base(goal, points, false)
+        {
+            
+        }
 
+        public override string GetDetailsString()
+        {
+            Console.WriteLine($"You gained {GetPoints()} points for {GetPoints()}!");
+        }
+        
+
+        public override void RecordEvent()
+        {
+
+        }
+
+
+
+    }
 
 
 
     
-}
+} 
