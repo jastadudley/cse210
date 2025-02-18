c2f1de16f8134364cdd0c11e68728917501ba3c8
jastadudley <jastamdudley@gmail.com>
2025-02-17 23:21:22 -0700

    Outline for files/classes and Cycling outline

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,47 @@
+namespace Activity
+{
+    public class Cycling() : Activity
+    {
+        private string _date;
+        private int _minute;
+        private int _speed;
+
+        public Cycling(string date, int minute, int speed)
+        :base()
+        {
+            _date = date;
+            _minute = minute;
+            _speed = speed;
+        }
+
+        public void GetDistance()
+        {
+
+        }
+
+        public void GetSpeed()
+        {
+
+        }
+
+        public void GetPace()
+        {
+
+        }
+
+        public void GetSummery()
+        {
+            
+        }
+
+
+    }
+
+
+
+
+
+
+
+    
+}
