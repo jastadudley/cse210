08ccd889b9d847e76f3a368afda1168661b0cd46
jastadudley <jastamdudley@gmail.com>
2025-02-20 11:43:05 -0700

    Outline for Swimming class

--- week07/ExerciseTracking/Swimming.cs
+++ week07/ExerciseTracking/Swimming.cs
@@ -1,6 +1,42 @@
 namespace Activity
 {
+    public class Swimming : Activity
+    {
+        private string _date;
+        private int _minutes;
+        private int _laps;
 
+        public Swimming(string date, int minutes, int laps)
+        :base()
+        {
+            _date = date;
+            _minutes = minutes;
+            _laps = laps;
+        }
+
+        public void GetSpeed()
+        {
+
+    
+        }
+
+        public void GetPace()
+        {
+
+        }
+
+        public void GetDistance()
+        {
+
+        }
+
+        public void GetSummery()
+        {
+            
+        }
+
+
+    }
 
 
 
