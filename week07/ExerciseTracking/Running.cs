8cac68c9126d691148b6a1571c772fc4a0e1aa5e
jastadudley <jastamdudley@gmail.com>
2025-02-20 11:38:58 -0700

    Outline for Running class

--- week07/ExerciseTracking/Running.cs
+++ week07/ExerciseTracking/Running.cs
@@ -1,5 +1,44 @@
 namespace Activity
 {
+    public class Running : Activity
+    {
+        private string _date;
+        private int _minutes;
+        private double _distance;
+
+        public Running(string date, int minutes, double distance)
+        :base()
+        {
+            _date = date;
+            _minutes = minutes;
+            _distance = distance;
+
+
+
+        }
+
+        public void GetDistance()
+        {
+
+        }
+
+        public void GetSpeed()
+        {
+
+        }
+
+        public void GetPace()
+        {
+
+        }
+
+        public void GetSummery()
+        {
+            
+        }
+
+    }
+
 
 
 
